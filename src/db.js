a43fa7f55d3b6c8d3d62abeb7430656ad75e1be2
cjsq24 <carlossosaq24@gmail.com>
2020-12-15 10:20:43 -0400

    conf

--- src/db.js
+++ src/db.js
@@ -1,5 +1,10 @@
 import Sequelize from 'sequelize';
 
+process.env.DB_USER = 'mZ6jUwPPFw'
+process.env.DB_PASSWORD = '4x2JIQietX'
+process.env.DB_NAME = 'mZ6jUwPPFw'
+process.env.DB_HOST = 'remotemysql.com'
+
 const sequelize = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`)
 
 export default sequelize;
